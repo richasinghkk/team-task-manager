c5d62fe6459ab53f256d0940e2e6e189e28ff309
Richa Singh <146985975+richasinghkk@users.noreply.github.com>
2026-05-19 19:26:26 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.options('*', cors());
+
 app.use(express.json());
 
 // Routes
