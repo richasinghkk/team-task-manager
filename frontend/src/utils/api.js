ac84c0d038d764ff3f85b03a6c029dfd78b54281
Richa Singh <146985975+richasinghkk@users.noreply.github.com>
2026-05-19 19:37:43 +0530

    commit

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://tender-surprise.up.railway.app/api'
+  baseURL: 'https://tender-surprise-production-8356.up.railway.app/api'
 });
 
 API.interceptors.request.use((req) => {
